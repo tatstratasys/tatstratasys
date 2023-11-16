79576b1ae545e7a6adb6877409ef0a95effa057a
tatstratasys <tatiana.ngalim@stratasys.com>
2023-11-16 13:27:16 +0000

    changes to key policy

--- main.tf
+++ main.tf
@@ -9,35 +9,60 @@ resource "aws_kms_key" "cmk" {
     Version: "2012-10-17",
     Id: "gc-ebs-policy",
     Statement: [
-      {
-        Sid: "Enable IAM User Permissions",
-        Effect: "Allow",
-        Principal: {
-          AWS: [
-            "arn:aws:iam::975732635630:root",
-          ]
+    {
+    "Version": "2012-10-17",
+    "Id": "gc-ebs-policy",
+    "Statement": [
+        {
+            "Sid": "Enable IAM User Permissions",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": [
+                    "arn:aws:iam::148285078518:root",
+                    "arn:aws:iam::625109008004:root"
+                ]
+            },
+            "Action": "kms:*",
+            "Resource": "*"
         },
-        Action: "kms:*",
-        Resource: "*"
-      },
-      {
-        Sid: "Enable EBS Encryption",
-        Effect: "Allow",
-        Principal: {
-          Service: "ec2.amazonaws.com"
+        {
+            "Sid": "Allow service-linked role use of the customer managed key",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": [
+                    "arn:aws:iam::625109008004:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
+                    "arn:aws:iam::625109008004:role/gc-eks-service-role-production-k8s-role"
+                ]
+            },
+            "Action": [
+                "kms:Encrypt",
+                "kms:Decrypt",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:DescribeKey"
+            ],
+            "Resource": "*"
         },
-        Action: "kms:Encrypt",
-        Resource: "*"
-      },
-      {
-        Sid: "Allow Key Rotation",
-        Effect: "Allow",
-        Principal: {
-          Service: "ec2.amazonaws.com"
-        },
-        Action: "kms:Rotate*",
-        Resource: "*"
-      }
+        {
+            "Sid": "Allow attachment of persistent resources",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": [
+                    "arn:aws:iam::625109008004:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
+                    "arn:aws:iam::625109008004:role/gc-eks-service-role-production-k8s-role",
+                    "arn:aws:iam::148285078518:root"
+                ]
+            },
+            "Action": "kms:CreateGrant",
+            "Resource": "*",
+            "Condition": {
+                "Bool": {
+                    "kms:GrantIsForAWSResource": "true"
+                }
+            }
+        }
+    ]
+}
     ]
   })
 }
@@ -50,3 +75,5 @@ resource "aws_kms_alias" "cmk_alias" {
 output "cmk_id" {
   value = aws_kms_key.cmk.id
 }
+
+
